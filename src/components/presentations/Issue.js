98f88c992ab4d528d11df6cdd0695c979c2866df
ihsebay <yabe.tomohisa@tis.co.jp>
2018-06-13 09:49:13 +0900

    "changes"

--- src/components/presentations/Issue.js
+++ src/components/presentations/Issue.js
@@ -10,7 +10,6 @@ export default class Issue extends Component {
 
   //constructor
   constructor(props) {
-    console.log("コンストラクター疎通！！！");
     super(props)
     const id = props.match.params.id
     this.state = {
@@ -68,7 +67,6 @@ export default class Issue extends Component {
   }
 
   summaryData = (Details_Data) => {
-    // console.log("Flagだよ : ", this.state.copyFlag);
     if(this.state.copyFlag === false) {
       if(Details_Data.length === 0) {
         // console.log("0行の場合")
@@ -114,32 +112,34 @@ export default class Issue extends Component {
       }
       else {
         // console.log("2行以上の場合")
-        let tmp_result_es04
-        let tmp_result_es05
-        let tmp_result_es06
-        let tmp_result_es07
-        let tmp_result_es08
-        let tmp_result_es09
-        let tmp_result_es10
-        let tmp_result_es11
-        let tmp_result_es12
-        let tmp_result_es01
-        let tmp_result_es02
-        let tmp_result_es03
-        const tmp = Details_Data.reduce((result, current) => {
-          tmp_result_es04 = result.es04 + current.es04
-          tmp_result_es05 = result.es05 + current.es05
-          tmp_result_es06 = result.es06 + current.es06
-          tmp_result_es07 = result.es07 + current.es07
-          tmp_result_es08 = result.es08 + current.es08
-          tmp_result_es09 = result.es09 + current.es09
-          tmp_result_es10 = result.es10 + current.es10
-          tmp_result_es11 = result.es11 + current.es11
-          tmp_result_es12 = result.es12 + current.es12
-          tmp_result_es01 = result.es01 + current.es01
-          tmp_result_es02 = result.es02 + current.es02
-          tmp_result_es03 = result.es03 + current.es03
-        })
+        let tmp_result_es04 = 0.0
+        let tmp_result_es05 = 0.0
+        let tmp_result_es06 = 0.0
+        let tmp_result_es07 = 0.0
+        let tmp_result_es08 = 0.0
+        let tmp_result_es09 = 0.0
+        let tmp_result_es10 = 0.0
+        let tmp_result_es11 = 0.0
+        let tmp_result_es12 = 0.0
+        let tmp_result_es01 = 0.0
+        let tmp_result_es02 = 0.0
+        let tmp_result_es03 = 0.0
+
+        for(let i = 0; i < Details_Data.length; i++) {
+          tmp_result_es04 += Details_Data[i].es04
+          tmp_result_es05 += Details_Data[i].es05
+          tmp_result_es06 += Details_Data[i].es06
+          tmp_result_es07 += Details_Data[i].es07
+          tmp_result_es08 += Details_Data[i].es08
+          tmp_result_es09 += Details_Data[i].es09
+          tmp_result_es10 += Details_Data[i].es10
+          tmp_result_es11 += Details_Data[i].es11
+          tmp_result_es12 += Details_Data[i].es12
+          tmp_result_es01 += Details_Data[i].es01
+          tmp_result_es02 += Details_Data[i].es02
+          tmp_result_es03 += Details_Data[i].es03
+        }
+
         this.setState({summary: {
           id: '合計',
           grade: "",
@@ -205,32 +205,33 @@ export default class Issue extends Component {
       }
       else {
         // console.log("2行以上の場合")
-        let tmp_result_es04
-        let tmp_result_es05
-        let tmp_result_es06
-        let tmp_result_es07
-        let tmp_result_es08
-        let tmp_result_es09
-        let tmp_result_es10
-        let tmp_result_es11
-        let tmp_result_es12
-        let tmp_result_es01
-        let tmp_result_es02
-        let tmp_result_es03
-        const tmp = Details_Data.reduce((result, current) => {
-          tmp_result_es04 = result.es04 + current.es04
-          tmp_result_es05 = result.es05 + current.es05
-          tmp_result_es06 = result.es06 + current.es06
-          tmp_result_es07 = result.es07 + current.es07
-          tmp_result_es08 = result.es08 + current.es08
-          tmp_result_es09 = result.es09 + current.es09
-          tmp_result_es10 = result.es10 + current.es10
-          tmp_result_es11 = result.es11 + current.es11
-          tmp_result_es12 = result.es12 + current.es12
-          tmp_result_es01 = result.es01 + current.es01
-          tmp_result_es02 = result.es02 + current.es02
-          tmp_result_es03 = result.es03 + current.es03
-        })
+        let tmp_result_es04 = 0.0
+        let tmp_result_es05 = 0.0
+        let tmp_result_es06 = 0.0
+        let tmp_result_es07 = 0.0
+        let tmp_result_es08 = 0.0
+        let tmp_result_es09 = 0.0
+        let tmp_result_es10 = 0.0
+        let tmp_result_es11 = 0.0
+        let tmp_result_es12 = 0.0
+        let tmp_result_es01 = 0.0
+        let tmp_result_es02 = 0.0
+        let tmp_result_es03 = 0.0
+
+        for(let i = 0; i < Details_Data.length; i++) {
+          tmp_result_es04 += Details_Data[i].es04
+          tmp_result_es05 += Details_Data[i].es05
+          tmp_result_es06 += Details_Data[i].es06
+          tmp_result_es07 += Details_Data[i].es07
+          tmp_result_es08 += Details_Data[i].es08
+          tmp_result_es09 += Details_Data[i].es09
+          tmp_result_es10 += Details_Data[i].es10
+          tmp_result_es11 += Details_Data[i].es11
+          tmp_result_es12 += Details_Data[i].es12
+          tmp_result_es01 += Details_Data[i].es01
+          tmp_result_es02 += Details_Data[i].es02
+          tmp_result_es03 += Details_Data[i].es03
+        }
         this.state.summary = {
           id: '合計',
           grade: "",
@@ -253,10 +254,10 @@ export default class Issue extends Component {
     }
   }
 
-  rowData = (id, issue_rows, group_users, changes) => {
-    console.log("rowDataだよ");
-    console.log("issue_rows : ",issue_rows);
-    console.log("this.state.copyFlag : ",this.state.copyFlag);
+  rowData = (id, issue_rows, group_users, changes, i) => {
+    // console.log("rowDataだよ");
+    // console.log("issue_rows : ",issue_rows);
+    // console.log("this.state.copyFlag : ",this.state.copyFlag);
     //オブジェクトの値渡し
     // console.log("Flag:", this.state.copyFlag);
     if(this.state.copyFlag === true) {
@@ -270,14 +271,13 @@ export default class Issue extends Component {
 
     //編集された場合にローカルステートを更新する
     if(changes !== undefined && changes !== null) {
-      eval("this.state.details[" + changes[0][0] + "]." + changes[0][1] + "=" + changes[0][3])
+        eval("this.state.details[" + changes[i][0] + "]." + changes[i][1] + "=" + changes[i][3])
     }
     //編集後の値で明細表示
     // let details_data
     // let summary_data
     // console.log("copyFlagだ : ", this.state.copyFlag);
     // console.log("changes : ", changes);
-
 
     if(this.state.copyFlag === true || changes !== null) {
       const details_data = this.state.details.map(row => {
@@ -316,34 +316,57 @@ export default class Issue extends Component {
       })
 
       const summary_data = this.summaryData(details_data)
-      const data = details_data.concat(summary_data)
+      // const data = details_data.concat(summary_data)
+      const data = details_data
       this.state.copyFlag = false
       // console.log("this.state.details : ",this.state.details)
       return data
     }
     else {
       // console.log("details : ", this.state.details);
-      return this.state.details.concat(this.state.summary)
+      // return this.state.details.concat(this.state.summary)
+      return this.state.details
     }
   }
 
   rowData0 = (id, issue_rows) => {
-    const guide = JSON.parse(JSON.stringify(issue_rows.filter(row => row.parent === id && row.id === id)))
-    return {
-      category: "山積工数",
-      es04: guide[0].es04,
-      es05: guide[0].es05,
-      es06: guide[0].es06,
-      es07: guide[0].es07,
-      es08: guide[0].es08,
-      es09: guide[0].es09,
-      es10: guide[0].es10,
-      es11: guide[0].es11,
-      es12: guide[0].es12,
-      es01: guide[0].es01,
-      es02: guide[0].es02,
-      es03: guide[0].es03
+    const _piling= JSON.parse(JSON.stringify(issue_rows.filter(row => row.parent === id && row.id === id)))
+    const piling = {
+      id: "山積工数",
+      es04: _piling[0].es04,
+      es05: _piling[0].es05,
+      es06: _piling[0].es06,
+      es07: _piling[0].es07,
+      es08: _piling[0].es08,
+      es09: _piling[0].es09,
+      es10: _piling[0].es10,
+      es11: _piling[0].es11,
+      es12: _piling[0].es12,
+      es01: _piling[0].es01,
+      es02: _piling[0].es02,
+      es03: _piling[0].es03
     }
+    return piling
+  }
+
+  rowData2 = (summary) => {
+    const _plans= JSON.parse(JSON.stringify(summary))
+    const plans = {
+      id: "予定工数",
+      es04: _plans.es04,
+      es05: _plans.es05,
+      es06: _plans.es06,
+      es07: _plans.es07,
+      es08: _plans.es08,
+      es09: _plans.es09,
+      es10: _plans.es10,
+      es11: _plans.es11,
+      es12: _plans.es12,
+      es01: _plans.es01,
+      es02: _plans.es02,
+      es03: _plans.es03
+    }
+    return plans
   }
 
   initInfo = (id,issue_rows) => {
@@ -454,7 +477,7 @@ export default class Issue extends Component {
       status: "none",
       copyFlag : true
     })
-    this.rowData(this.state.id, this.props.issue_rows, this.props.group_users, null)
+    this.rowData(this.state.id, this.props.issue_rows, this.props.group_users, null, null)
   }
 
   onChange1 = (event, key, payload) => {
@@ -474,16 +497,19 @@ export default class Issue extends Component {
       // 変更された要素をlocalState.change_dataに保存
       // 画面で同一項目が複数回更新され場合はlocalState.change_dataを上書き
       // console.log("onChange2が呼び出しされた")
-      // console.log("changesとは：",changes)
       for (let i in changes) {
+        if(changes[i][3] === "") {
+          changes[i][3] = 0.0
+        }
         const change_row = changes[i][0]
         const change_key = changes[i][1]
         const change_value = changes[i][3]
         //issue_rows全体から案件登録画面に表示されている案件の子チケットのみを絞り込む
 
-        const row_data = this.rowData(this.state.id, this.props.issue_rows, this.props.groupUsers, changes)
+        const row_data = this.rowData(this.state.id, this.props.issue_rows, this.props.groupUsers, changes, i)
         // console.log("this.state.summary:",this.state.summary);
         // localState.change_dataが空の場合は無条件で要素を追加
+
         if(this.state.change_data.id.length === 0){
           this.state.change_data.id.push(row_data[change_row]["id"])
           this.state.change_data.key.push(change_key)
@@ -597,7 +623,7 @@ export default class Issue extends Component {
 
   //カラムデータ定義(山積＆作業工数の算出)
   columns0 = [
-    { data: 'category', readOnly: true, width: 310 },
+    { data: 'id', readOnly: true, width: 310 },
     { data: 'es04', type: 'numeric', allowInvalid: false, format: '0.00', width: 50 },
     { data: 'es05', type: 'numeric', allowInvalid: false, format: '0.00', width: 50 },
     { data: 'es06', type: 'numeric', allowInvalid: false, format: '0.00', width: 50 },
@@ -706,7 +732,6 @@ export default class Issue extends Component {
               <HotTable
                 floatingLabelText={<span style={{fontSize: 16}}>山積＆作業工数の算出</span>}
                 root="hot0"
-                //functionを新たに作る
                 data={this.rowData0(this.state.id, this.props.issue_rows)}
                 colHeaders={this.colHeaders0}
                 columns={this.columns0}
@@ -715,7 +740,20 @@ export default class Issue extends Component {
                 width="910"
                 stretchH="all"
                 fixedColumnsLeft="3"
-                manualColumnResize={true}
+                manualColumnResize={false}
+                fillHandle={false}
+                />
+              <HotTable
+                floatingLabelText={<span style={{fontSize: 16}}>山積＆作業工数の算出</span>}
+                root="hot3"
+                data={this.rowData2(this.state.summary)}
+                columns={this.columns0}
+                columnSorting={true}
+                readOnly={true}
+                width="910"
+                stretchH="all"
+                fixedColumnsLeft="3"
+                manualColumnResize={false}
                 fillHandle={false}
                 />
             </div>
@@ -725,16 +763,30 @@ export default class Issue extends Component {
               <HotTable
                 floatingLabelText={<span style={{fontSize: 16}}>要員計画</span>}
                 root="hot1"
-                data={this.rowData(this.state.id, this.props.issue_rows, this.props.groupUsers, null)}
+                data={this.rowData(this.state.id, this.props.issue_rows, this.props.groupUsers, null, null)}
                 colHeaders={this.colHeaders}
                 columns={this.columns}
                 columnSorting={true}
                 width="910"
                 stretchH="all"
                 fixedColumnsLeft="3"
-                manualColumnResize={true}
+                manualColumnResize={false}
                 fillHandle={false}
                 afterChange={this.onChange2}
+                columnSorting={true}
+                />
+              <HotTable
+                floatingLabelText={<span style={{fontSize: 16}}>予定工数</span>}
+                root="hot2"
+                data={this.state.summary}
+                columns={this.columns0}
+                columnSorting={true}
+                readOnly={true}
+                width="910"
+                stretchH="all"
+                fixedColumnsLeft="3"
+                manualColumnResize={false}
+                fillHandle={false}
                 />
             </div>
             <Link to='/issue'>
@@ -764,7 +816,7 @@ export default class Issue extends Component {
             >
             </Dialog>
             <Dialog
-              title="addMember"
+              title="要員追加"
               actions={actionSubmit}
               modal={true}
               open={this.state.status === "inputing"}
