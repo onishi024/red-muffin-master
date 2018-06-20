3f989da5c809720c78e63c86cdbf6d5cb009cc88
imim10042 <orange10042@gmail.com>
2018-06-20 18:29:38 +0900

    bug fix(toggle,refresh)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -93,13 +93,13 @@ const reducer = (state = initState, action) => {
     case ActionTypes.TOGGLE_ISSUE_HIDE: {
       const current_id = action.payload.id
       const toggled_bool = action.payload.bool
-      const issue_rows = state.issue_rows.map(issue_row => {
-        if (issue_row.id === current_id) {
-          issue_row.hide = toggled_bool
+      const parent_issue_rows = state.parent_issue_rows.map(parent_issue_row => {
+        if (parent_issue_row.id === current_id) {
+          parent_issue_row.hide = toggled_bool
         }
-        return issue_row
+        return parent_issue_row
       })
-      return {...state, issue_rows, current_id}
+      return {...state, parent_issue_rows, current_id}
     }
     case ActionTypes.ONOFF_SNACKBAR: {
       const snackbar_open = !state.snackbar_open
